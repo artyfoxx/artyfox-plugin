115b11bf848b7cb5b09f6f4ca0a1ee74bcd797c1
artyfox <artyfox@yandex.ru>
2026-06-20 20:36:37 +0600

    Повышена версия

--- src/artyfox.c
+++ src/artyfox.c
@@ -7592,7 +7592,7 @@ static void VS_CC AverageFieldsCreate(
 }
 
 VS_EXTERNAL_API(void) VapourSynthPluginInit2(VSPlugin *plugin, const VSPLUGINAPI *vspapi) {
-    vspapi->configPlugin("ru.artyfox.plugins", "artyfox", "A disjointed set of filters", VS_MAKE_VERSION(19, 0), VAPOURSYNTH_API_VERSION, 0, plugin);
+    vspapi->configPlugin("ru.artyfox.plugins", "artyfox", "A disjointed set of filters", VS_MAKE_VERSION(20, 0), VAPOURSYNTH_API_VERSION, 0, plugin);
     vspapi->registerFunction("Resize",
                              "clip:vnode;"
                              "width:int;"
